d0d2daa55e7d20bd2c979fde0877bb19eee5cff3
Julian0816 <83423059+Julian0816@users.noreply.github.com>
2023-04-02 00:15:33 +0100

    nav bar started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route element={<Login />} path="/" />
         </Routes>
